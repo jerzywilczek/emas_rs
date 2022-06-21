4dbf585ca196e070d8d018d09d32ddad5c632303
jerzywilczek <jerzykwilczek@gmail.com>
2022-06-21 21:09:31 +0200

    Modify rastrigin_example to showcase const generic dimensions in our rastrigin function implementation

--- examples/rastrigin_example.rs
+++ examples/rastrigin_example.rs
@@ -3,10 +3,22 @@ use emas_rs::fitness_functions::{RastriginFitness, FitnessFn};
 use std::time::Instant;
 
 fn main() {
-    let mut system = SystemBuilder::<2, RastriginFitness<2>>::new().steps(10_000).build();
+    const N: usize = 100;
+    let mut system = SystemBuilder::<N, RastriginFitness<N>>::new().steps(1_000_000).build();
     let t0 = Instant::now();
     let sol = system.run();
     let t0 = t0.elapsed().as_secs_f32();
-    println!("[{}, {}] => {}", sol[0], sol[1], RastriginFitness::call(&sol));
+
+    print!("[");
+    let mut first = true;
+    for arg in sol {
+        if first {
+            print!("{}", arg);
+            first = false;
+        } else {
+            print!(", {}", arg);
+        }
+    }
+    println!("] => {}", RastriginFitness::call(&sol));
     println!("{}s", t0);
 }
